44d8aa7f95fa62a9ff251ce628a99c085bfddf14
Carlos Soto <carlosdedios0621@gmail.com>
2024-07-16 19:43:09 -0400

    modificando lambda

--- lambda/main.py
+++ lambda/main.py
@@ -1,8 +1,11 @@
 
 
 def handler(event, context):
-    
+    response_body = {
+        "message": "Hello World",
+        "version": "1.0"
+    }
     return {
         'statusCode': 200,
-        'body': 'Hello, World!'
+        'body': response_body
     }
