158a2af02c37ed8b18035d89fde16810f1e21994
Carlos Soto <carlosdedios0621@gmail.com>
2024-07-16 20:51:09 -0400

    add vistor count

--- lib/aws-cicd-tutorial-stack.ts
+++ lib/aws-cicd-tutorial-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamo from "aws-cdk-lib/aws-dynamodb";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as dotenv from "dotenv";
 
@@ -10,14 +11,27 @@ export class AwsCicdTutorialStack extends cdk.Stack {
 
     dotenv.config();
 
+    // Create a table to store some data
+    const table = new dynamo.Table(this, "VisitorTimeTable", {
+      partitionKey: {
+        name: "key",
+        type: dynamo.AttributeType.STRING,
+      },
+      billingMode: dynamo.BillingMode.PAY_PER_REQUEST,
+    });
+
+    // region
     const lambdaFunction = new lambda.Function(this, "LambdaFunction", {
       runtime: lambda.Runtime.PYTHON_3_11,
       code: lambda.Code.fromAsset("lambda"),
       handler: "main.handler",
       environment: {
         VERSION: process.env.VERSION || "1.0.0",
+        TABLE_NAME: table.tableName,
       },
     });
+
+    table.grantReadWriteData(lambdaFunction);
 
     const functionUrl = lambdaFunction.addFunctionUrl({
       authType: lambda.FunctionUrlAuthType.NONE,
@@ -27,7 +41,7 @@ export class AwsCicdTutorialStack extends cdk.Stack {
         allowedHeaders: ["*"],
       },
     });
-
+    // endregion
     new cdk.CfnOutput(this, "FunctionUrl", {
       value: functionUrl.url,
     });
